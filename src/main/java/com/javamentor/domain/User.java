2bb65914f6bae893e3fe55385ae56d1f62f45e00
ig.ezhov@mail.ru <xX1210!(*!>
2020-11-05 17:49:10 +0300

    Коммит 2

---
+++ src/main/java/com/javamentor/domain/User.java
@@ -0,0 +1,93 @@
+package com.javamentor.domain;
+
+import java.time.LocalDateTime;
+
+public class User {
+
+    Long id;
+
+    String email;
+
+
+    String firstName;
+
+
+    String lastName;
+
+
+    String password;
+
+
+    Boolean enabled = true;
+
+    LocalDateTime registrationDate;
+
+    public User(String email, String firstName, String lastName, String password, Boolean enabled, LocalDateTime registrationDate) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.enabled = enabled;
+        this.registrationDate = registrationDate;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+}
