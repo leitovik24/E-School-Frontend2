3220c246090307e78afdc66ce5c5a498bd6b6543
ig.ezhov@mail.ru <xX1210!(*!>
2020-11-06 17:07:39 +0300

    Реализация таблицы студентов

---
+++ src/main/java/com/javamentor/service/StudentService.java
@@ -0,0 +1,37 @@
+package com.javamentor.service;
+
+import com.javamentor.domain.Teacher;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Service
+public class StudentService {
+    String URL = "http://localhost:8080/api/admin/student/students";
+    Logger LOGGER = Logger.getLogger(TeacherService.class.getName());
+
+    public List<Teacher> getAll() {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<List<Teacher>> response =
+                    restTemplate.exchange(URL,
+                            HttpMethod.GET, null, new ParameterizedTypeReference<List<Teacher>>() {
+                            });
+            if (response.getStatusCode().equals(HttpStatus.OK)) {
+                return response.getBody();
+            }
+        } catch (HttpClientErrorException e) {
+            LOGGER.log(Level.WARNING, e.getResponseBodyAsString());
+        }
+        return Collections.emptyList();
+    }
+}
---
+++ src/main/java/com/javamentor/view/StudentList.java
@@ -0,0 +1,32 @@
+package com.javamentor.view;
+
+import com.javamentor.domain.Teacher;
+import com.javamentor.service.StudentService;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+@Route(value = "/admin/student")
+@Theme(Lumo.class)
+public class StudentList extends VerticalLayout {
+    private final StudentService service = new StudentService();
+    private final Grid<Teacher> grid = new Grid<>(Teacher.class);
+
+    public StudentList() {
+        grid.setColumns("email", "firstName", "lastName", "password", "registrationDate");
+        grid.getColumnByKey("email").setHeader("E-mail");
+        grid.getColumnByKey("firstName").setHeader("Имя");
+        grid.getColumnByKey("lastName").setHeader("Фамилия");
+        grid.getColumnByKey("password").setHeader("Пароль");
+        grid.getColumnByKey("registrationDate").setHeader("Дата регистрации");
+        add(grid);
+        setSizeFull();
+        updateList();
+    }
+
+    public void updateList() {
+        grid.setItems(service.getAll());
+    }
+}
